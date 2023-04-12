13ed8d980e69c0ed41428f1aadbc6dfed4841e6b
Hendrik Roehm <git@roehm.ws>
2023-04-12 22:39:47 +0200

    Add SimpleOutcome and plotting

--- src/probability_calculator/DiscreteDensity.py
+++ src/probability_calculator/DiscreteDensity.py
@@ -1,3 +1,46 @@
+import matplotlib.pyplot as plt
+
 
 class DiscreteDensity:
-    pass
+    def __init__(self, outcomes=[]):
+        self._outcomes = []
+
+        for o in outcomes:
+            self._outcomes.append(
+                SimpleOutcome(value=o["value"], prob=o["prob"])
+            )
+
+    def plot(self):
+        X, Y = self._getPlotData()
+
+        fig1, ax = plt.subplots()
+        ax.plot(X, Y, "o")
+        ax.set_ylim(bottom=0)
+        plt.show()
+        return fig1, ax
+
+    def _getPlotData(self, precision=1e-6):
+        points = []
+
+        for o in self._outcomes:
+            points.append((o.value, o.prob))
+
+        X = []
+        Y = []
+        lastValue = None
+        for value, prob in sorted(points):
+            if lastValue is not None and value - lastValue < precision:
+                # multiple times the same point -> add probabilities together
+                Y[-1] += prob
+            else:
+                X.append(value)
+                Y.append(prob)
+                lastValue = value
+
+        return X, Y
+
+
+class SimpleOutcome:
+    def __init__(self, value, prob):
+        self.value = value
+        self.prob = prob
--- tests/test_DiscreteDensity.py
+++ tests/test_DiscreteDensity.py
@@ -1,5 +1,23 @@
 from probability_calculator.DiscreteDensity import DiscreteDensity
 
-def test_class():
-    d = DiscreteDensity()
-    assert d
+
+class TestDiscreteDensity():
+    def test_getPlotData(self):
+        d = DiscreteDensity(outcomes=[
+            {"value": 1, "prob": 1./6},
+            {"value": 2, "prob": 1./3},
+            {"value": 1, "prob": 1./6},
+            {"value": 3, "prob": 1./3}
+        ])
+        X, Y = d._getPlotData()
+        assert X == [1, 2, 3]
+        assert Y == [1./3, 1./3, 1./3]
+
+    def test_plot(self):
+        d = DiscreteDensity(outcomes=[
+            {"value": 1, "prob": 1./6},
+            {"value": 2, "prob": 1./3},
+            {"value": 1, "prob": 1./6},
+            {"value": 3, "prob": 1./3}
+        ])
+        d.plot()
